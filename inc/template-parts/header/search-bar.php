00489b8b61667b1fd737fed12e783997403191c1
Asadullah Galib <md.asadullah196@gmail.com>
2024-05-19 11:42:28 +0800

    Update search code

--- inc/template-parts/header/search-bar.php
+++ inc/template-parts/header/search-bar.php
@@ -1,8 +1,8 @@
  <!-- navigation drop search bar -->
  <div class="search" id="search-bar">
-    <form class="d-flex nav-search" action="/" method="get">
-        <input type="text" name="search" placeholder="Enter your text" id="search" value="<?php the_search_query(); ?>">
-        <button class="btn-hover1" type="submit">Search</button>
+    <form role="search" class="search-form d-flex nav-search" action="/" method="get">
+        <input type="text" name="s" placeholder="Enter your text" id="search" value="<?php the_search_query(); ?>">
+        <button class="btn-hover1 search-submit" type="submit">Search</button>
     </form>
     <button id="remove-btn">
         <i class="fa-solid fa-square-xmark"></i>
---
+++ search.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+<!-- ======== 9.1. Blogs cards section ========  -->
+<section class="news-cards search">
+    <div class="container">
+        <div class="row d-flex gap-md-0 gap-sm-5 gap-4 mb-5">
+        <!-- blog post start --> 
+        <?php if ( have_posts() ) : ?>
+            <?php while( have_posts()  ) : the_post(); ?>
+                <?php echo get_template_part( 'inc/template-parts/content' , get_post_format() ); ?>
+            <?php endwhile; ?>
+        <?php endif; ?>
+        <!-- blog post end --> 
+        </div>
+    </div>
+</section>
+<!-- ======== End of 9.1. Blogs cards section ========  -->
+<?php get_footer(); ?>
